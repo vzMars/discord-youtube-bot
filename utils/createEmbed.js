ef87bfb7cad4765173ab9a98e5fb1fcc0e40d9ad
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-02-18 14:28:31 -0500

    function that creates embeds that notify users when new video is uploaded

---
+++ utils/createEmbed.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require('discord.js');
+
+const createEmbed = (client, video) => {
+  const embed = new EmbedBuilder()
+    .setColor('Red')
+    .setTitle(video.title)
+    .setURL(`https://www.youtube.com/watch?v=${video.videoId}`)
+    .setAuthor({
+      name: video.channelTitle,
+      iconURL: video.icon,
+      url: `https://www.youtube.com/channel/${video.channelId}`,
+    })
+    .setThumbnail(video.icon)
+    .setImage(video.thumbnail)
+    .setTimestamp();
+
+  const channel = client.channels.cache.get(video.textChannelID);
+  channel.send({ embeds: [embed] });
+};
+
+module.exports = {
+  createEmbed,
+};
