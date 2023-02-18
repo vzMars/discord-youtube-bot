2a95f11ef918b7470d943f21aa4b698fd5fd90c0
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-02-18 14:30:33 -0500

    checks for any new videos every min using node-schedule and if there is sends a embed message

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
 const { Events } = require('discord.js');
+const { fetchLatestVideos } = require('../utils/fetchYoutube');
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    fetchLatestVideos(client);
   },
 };
--- utils/fetchYoutube.js
+++ utils/fetchYoutube.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
+const schedule = require('node-schedule');
+const Channel = require('../models/Channel');
+const { createEmbed } = require('../utils/createEmbed');
 
 const baseUrl = 'https://youtube.googleapis.com/youtube/v3/';
 const apiKey = process.env.YOUTUBE_API_KEY;
 
 const fetchUploadsPlaylist = async (id) => {
-  const url = `${baseUrl}channels?part=contentDetails&id=${id}&key=${apiKey}`;
+  const url = `${baseUrl}channels?part=snippet&part=contentDetails&id=${id}&key=${apiKey}`;
   try {
     const response = await axios.get(url);
     const data = response.data;
@@ -14,12 +17,51 @@ const fetchUploadsPlaylist = async (id) => {
       return null;
     }
 
-    return data.items[0].contentDetails.relatedPlaylists.uploads;
+    const { uploads } = data.items[0].contentDetails.relatedPlaylists;
+    const icon = data.items[0].snippet.thumbnails.high.url;
+
+    return { uploads, icon };
   } catch (err) {
     console.error(err);
   }
 };
 
+const fetchLatestVideos = (client) => {
+  schedule.scheduleJob('* * * * *', async () => {
+    try {
+      const channels = await Channel.find();
+      for (const channel of channels) {
+        const videoUrl = `${baseUrl}playlistItems?part=snippet&playlistId=${channel.uploadPlaylistID}&key=${apiKey}`;
+        const response = await axios.get(videoUrl);
+        const { snippet } = response.data.items[0];
+        const { title, channelTitle, channelId } = snippet;
+        const { url } = snippet.thumbnails.maxres;
+        const { videoId } = snippet.resourceId;
+
+        const video = {
+          title,
+          channelTitle,
+          channelId,
+          icon: channel.icon,
+          thumbnail: url,
+          videoId,
+          textChannelID: channel.textChannelID,
+        };
+
+        if (channel.videoID !== video.videoId) {
+          channel.videoID = video.videoId;
+          await channel.save();
+
+          createEmbed(client, video);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
 module.exports = {
   fetchUploadsPlaylist,
+  fetchLatestVideos,
 };
